62c5ccd3c246635d0543802a2a022189154a58c0
Blaze2595 <balajiblaze2595@gmail.com>
2026-02-13 11:23:59 +0530

    Day 4 : Objs and Scripts

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,25 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename, select = [],types = []):
+    '''Parse a csv file into list of records'''
+
+    with open(filename,'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        records = []
+        for line in rows:
+            if not line:
+                continue
+            if(select and not types):
+                records.append({name:value for name,value in zip(headers,line) if name in select})
+            elif(select and types):
+                records.append({name:func(value) for name,value,func in zip(headers,line,types) if name in select})
+            elif(types and not select):
+                records.append({name:func(value) for name,value,func in zip(headers,line,types)})
+            else:
+                records.append({name:value for name,value in zip(headers,line)})
+    
+    return records
--- Work/report.py
+++ Work/report.py
@@ -30,10 +30,17 @@ def read_prices(filename):
 
     return values
 
-def make_report(portfolio, prices):
+def portfolio_report(portfoliofile, pricesfile):
     result = []
-    headers = ('Names','Share','Price','Cost')
-    line = ('__________','__________','__________','__________')
+
+    portfolio = read_portfolio(portfoliofile)
+    prices = read_prices(pricesfile)
+    result = get_profit_margin(portfolio,prices)
+    print_report(result)
+    return True
+
+def get_profit_margin(portfolio,prices):
+    result = []
 
     for item in portfolio:
         if(item['name'] in prices):
@@ -45,11 +52,14 @@ def make_report(portfolio, prices):
 
             result.append(t)
     
-    
+    return result
+
+def print_report(report):
+    headers = ('Names','Share','Price','Cost')
+    line = ('__________','__________','__________','__________')
+
     print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
     print(f'{line[0]:>10s} {line[1]:>10s} {line[2]:>10s} {line[3]:>10s}')
     
-    for _name,_share,_price,_change in result:
+    for _name,_share,_price,_change in report:
         print(f'{_name:>10s} {_share:>10d} {f"${_price:.2f}":>10} {_change:>10.2f}')
-
-    return True
